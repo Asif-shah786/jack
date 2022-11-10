a2575a6bd0aa0bd70521b0f892b083549954c394
Asif shah <asif.shah9091@gmail.com>
2022-11-10 19:17:51 +0500

    addded

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -2,7 +2,9 @@ import 'package:avatar_glow/avatar_glow.dart';
 import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:highlight_text/highlight_text.dart';
 import 'package:lottie/lottie.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 
 import '../api/speech_api.dart';
@@ -16,8 +18,55 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String text = 'Press the button and start speaking';
-  bool isListening = false;
+  String _text = 'Press the button and start speaking';
+  bool _isListening = false;
+  final Map<String, HighlightedWord> _highlights = {
+    'flutter': HighlightedWord(
+      onTap: () => print('flutter'),
+      textStyle: const TextStyle(
+        color: Colors.blue,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    'voice': HighlightedWord(
+      onTap: () => print('voice'),
+      textStyle: const TextStyle(
+        color: Colors.green,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    'subscribe': HighlightedWord(
+      onTap: () => print('subscribe'),
+      textStyle: const TextStyle(
+        color: Colors.red,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    'like': HighlightedWord(
+      onTap: () => print('like'),
+      textStyle: const TextStyle(
+        color: Colors.blueAccent,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    'comment': HighlightedWord(
+      onTap: () => print('comment'),
+      textStyle: const TextStyle(
+        color: Colors.green,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  };
+
+  late stt.SpeechToText _speech;
+  double _confidence = 1.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _speech = stt.SpeechToText();
+  }
+
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -29,8 +78,7 @@ class _HomePageState extends State<HomePage> {
               builder: (context) => IconButton(
                 icon: Icon(Icons.content_copy),
                 onPressed: () async {
-                  await FlutterClipboard.copy(text);
-
+                  await FlutterClipboard.copy(_text);
                   ScaffoldMessenger.of(context).showSnackBar(
                     SnackBar(content: Text('✓   Copied to Clipboard')),
                   );
@@ -39,52 +87,98 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
-        body: SingleChildScrollView(
-          reverse: true,
-          padding: const EdgeInsets.all(30).copyWith(bottom: 150),
-          child: Column(
-            children: [
-              SubstringHighlight(
-                text: text,
-                terms: Command.all,
-                textStyle: const TextStyle(
-                  fontSize: 32.0,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w400,
+        body: SizedBox(
+          width: Get.width,
+          height: Get.height * 0.7,
+          child: SingleChildScrollView(
+            reverse: true,
+            padding: const EdgeInsets.all(30).copyWith(bottom: 150),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: Get.width,
+                  height: Get.height * 0.3,
+                  child: SubstringHighlight(
+                    text: _text,
+                    terms: Command.all,
+                    textStyle: const TextStyle(
+                      fontSize: 32.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    textStyleHighlight: const TextStyle(
+                      fontSize: 32.0,
+                      color: Colors.red,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
                 ),
-                textStyleHighlight: const TextStyle(
-                  fontSize: 32.0,
-                  color: Colors.red,
-                  fontWeight: FontWeight.w400,
+                SizedBox(height: Get.height * 0.02,),
+                Lottie.asset("assets/lf20_g1pduE.json",
+                    fit: BoxFit.contain,height: 200,width: 200),
+                Text('Jack\'s Confidence: ${(_confidence * 100.0).toStringAsFixed(1)}%',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w400,
+                  ),
                 ),
-              ),
-              SizedBox(height: Get.height/4,),
-              Lottie.asset("assets/lf20_g1pduE.json",fit: BoxFit.contain,height: 200,width: 200),
-            ],
+              ],
+            ),
           ),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         floatingActionButton: AvatarGlow(
-          animate: isListening,
-          endRadius: 75,
           glowColor: Theme.of(context).primaryColor,
+          animate: _isListening,
+          endRadius: 75.0,
+          duration: const Duration(milliseconds: 2000),
+          repeatPauseDuration: const Duration(milliseconds: 100),
+          repeat: true,
           child: FloatingActionButton(
-            child: Icon(isListening ? Icons.mic : Icons.mic_none, size: 36),
-            onPressed: toggleRecording,
+            onPressed: _listen,
+            child: Icon(_isListening ? Icons.mic : Icons.mic_none),
           ),
         ),
       );
 
   Future toggleRecording() => SpeechApi.toggleRecording(
-        onResult: (text) => setState(() => this.text = text),
+        onResult: (text) => setState(() => this._text = text),
         onListening: (isListening) {
-          setState(() => this.isListening = isListening);
+          setState(() => this._isListening = isListening);
 
           if (!isListening) {
             Future.delayed(Duration(seconds: 1), () {
-              Utils.scanText(text);
+              Utils.scanText(_text);
             });
           }
         },
       );
+
+  void _listen() async {
+    if (!_isListening) {
+      bool available = await _speech.initialize(
+        onStatus: (val) => print('onStatus: $val'),
+        onError: (val) => print('onError: $val'),
+      );
+      if (available) {
+        setState(() => _isListening = true);
+        _speech.listen(
+          onDevice: false,
+          onResult: (val) => setState(() {
+            _text = val.recognizedWords;
+            if (val.hasConfidenceRating && val.confidence > 0) {
+              _confidence = val.confidence;
+            }
+          }),
+        );
+      }
+    } else {
+      setState(() => _isListening = false);
+      _speech.stop();
+    }
+  }
 }
